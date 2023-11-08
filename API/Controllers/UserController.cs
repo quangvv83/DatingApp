79962cad5298b5ce5e8a04098130e0e2bfd9a376
AKB-QuangVV12 <quangvv6@fpt.com>
2023-11-08 11:06:57 +0700

    .

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<AppUser>> GetUser(int id)
+        {
+            var user = _context.Users.Find(id);
+            return Ok(user);
+        }
+
+    }
+}

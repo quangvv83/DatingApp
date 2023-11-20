93873774fe286c23bb6abaf8b29ba99b77360ab4
Vu Van Quang <quang282@yahoo.com>
2023-11-20 10:54:06 +0700

    .

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using API.Data;
+
+namespace API;
+
+public class AccountController : BaseApiController
+{
+    private readonly DataContext _context;
+
+    public AccountController(DataContext context)
+    {
+        _context = context;
+    }
+
+}

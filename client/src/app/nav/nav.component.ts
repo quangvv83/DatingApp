fb187ca3c07ee3d1c7194838ba30705b040048cf
AKB-QuangVV12 <quangvv6@fpt.com>
2023-12-27 19:37:22 +0700

    .

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -9,28 +11,18 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
 
   model: any = {};
-  loggedIn = false;
-
-  constructor(private accountService: AccountService) {
+  constructor(public accountService: AccountService) {
 
   }
 
   ngOnInit(): void {
-    this.accountService.currentUser$.subscribe({
-      next: user => this.loggedIn = !!user,
-      error: error => console.log(error),
-    });
-  }
-
-  getCurrentUser() {
-
+    
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
       },
       error: error => console.log(error),
     })
@@ -38,7 +30,6 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
   }
 
 }

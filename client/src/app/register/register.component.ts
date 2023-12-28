3ce8ec572a88c120461cca61c5099c4fd7dad7eb
AKB-QuangVV12 <quangvv6@fpt.com>
2023-12-28 11:23:13 +0700

    .

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  model: any = {};
+
+  ngOnInit(): void {
+    
+  }
+
+  register() {
+    console.log("register");
+  }
+
+  cancel() {
+    console.log("cancelled");
+  }
 
 }

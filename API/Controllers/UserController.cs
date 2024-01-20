19e88a31f5fb29fa9dd7ea1ce8876c911db82ebb
Vu Van Quang <quang282@yahoo.com>
2024-01-20 22:13:03 +0700

    .

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -18,7 +18,6 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             var users = await _context.Users.ToListAsync();

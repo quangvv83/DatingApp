7b8630594ab24f1073230b79f2d905f2fe10c103
Vu Van Quang <quang282@yahoo.com>
2024-01-28 12:33:12 +0700

    .

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
 
 
@@ -10,11 +11,13 @@ import { ToastrModule } from 'ngx-toastr';
   imports: [
     CommonModule,
     BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
     ToastrModule.forRoot()
   ],
   exports: [
     BsDropdownModule,
     ToastrModule,
+    TabsModule,
   ],
 })
 export class SharedModule { }

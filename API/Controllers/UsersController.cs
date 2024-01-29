480bc3b65ec5f973c075311a6bd1425082d768f7
AKB-QuangVV12 <quangvv6@fpt.com>
2024-01-29 15:28:26 +0700

    .

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -32,11 +33,27 @@ namespace API.Controllers
         //    var userToReturn = _mapper.Map<MemberDto>(user);
         //    return userToReturn;
         //}
-        
+
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            if (user == null) { return NotFound(); }
+            _mapper.Map(memberUpdateDto, user);
+
+            if (await _userRepository.SavaAllAsync())
+            {
+                return NoContent();
+            }
+            return BadRequest("Failed to update user");
         }
 
     }

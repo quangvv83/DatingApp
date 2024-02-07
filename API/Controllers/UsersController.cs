0ad461adc6a54ade153306078e6a19e9dd6c2bf2
AKB-QuangVV12 <quangvv6@fpt.com>
2024-02-07 14:52:55 +0700

    .

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -83,7 +83,10 @@ namespace API.Controllers
             if (user.Photos.Count == 0) photo.IsMain = true;
             user.Photos.Add(photo);
 
-            if (await _userRepository.SaveAllAsync()) return _mapper.Map<PhotoDto>(photo);
+            if (await _userRepository.SaveAllAsync())
+            {
+                return CreatedAtAction(nameof(GetUser), new { user.UserName }, _mapper.Map<PhotoDto>(photo));
+            }
             return BadRequest("Error uploading photo");
         }
     }

79578e237ce82f9f4c430953775d0084857de20e
AKB-QuangVV12 <quangvv6@fpt.com>
2024-02-21 16:27:28 +0700

    .

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -25,11 +26,10 @@ namespace API.Data
                 .FirstOrDefaultAsync();
         }
 
-        public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+        public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
         {
-            return await _context.Users
-                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-                .ToListAsync();
+            var query = _context.Users.ProjectTo<MemberDto>(_mapper.ConfigurationProvider).AsNoTracking();
+            return await PagedList<MemberDto>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<AppUser> GetUserByIdAsync(int id)
@@ -54,7 +54,7 @@ namespace API.Data
 
         public void Update(AppUser user)
         {
-            _context.Entry(user).State = EntityState.Modified; 
+            _context.Entry(user).State = EntityState.Modified;
         }
     }
 }
---
+++ API/Extensions/HttpExtensions.cs
@@ -0,0 +1,15 @@
+﻿using API.Helpers;
+using System.Text.Json;
+
+namespace API.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse respone, PaginationHeader header )
+        {
+            var jsonOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            respone.Headers.Add("Pagination", JsonSerializer.Serialize(header, jsonOptions));
+            respone.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+    }
+}

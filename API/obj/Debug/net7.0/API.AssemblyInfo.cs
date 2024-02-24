69ca58868d208bd19a990ffa50b85ca347da8558
Vu Van Quang <quang282@yahoo.com>
2024-02-24 21:57:35 +0700

    .

--- API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c9c0597edd8850c4305f71217e9091814c08785")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+270ce72c688bc79ed04123c97a3286c5fd319ba8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

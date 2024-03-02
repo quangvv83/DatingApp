d85e37458ff5b3057dd67685ac9942f55802e094
AKB-QuangVV12 <quangvv6@fpt.com>
2024-03-02 11:36:03 +0700

    .

---
+++ API/Data/LikesRepository.cs
@@ -0,0 +1,59 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Helpers;
+using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class LikesRepository : ILikesRepository
+    {
+        private readonly DataContext _context;
+
+        public LikesRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserLike> GetUserLike(int sourceUserId, int targetUserId)
+        {
+            return await _context.Likes.FindAsync(sourceUserId, targetUserId);
+        }
+
+        public async Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+        {
+            var users = _context.Users.OrderBy(u => u.UserName).AsQueryable();
+            var likes = _context.Likes.AsQueryable();
+
+            if (predicate == "liked")
+            {
+                likes = likes.Where(l => l.SourceUserId == userId);
+                users = likes.Select(l => l.TargetUser);
+            }
+
+            if (predicate == "likedBy")
+            {
+                likes = likes.Where(l => l.TargetUserId == userId);
+                users = likes.Select(l => l.SourceUser);
+            }
+
+            return await users.Select(u => new LikeDto
+            {
+                UserName = u.UserName,
+                KnownAs = u.KnownAs,
+                Age = u.DateOfBirth.CalculateAge(),
+                PhotoUrl = u.Photos.FirstOrDefault(p => p.IsMain).Url,
+                City = u.City,
+                Id = u.Id,
+            }).ToListAsync();
+        }
+
+        public async Task<AppUser> GetUserWithLikes(int userId)
+        {
+            return await _context.Users.Include(u => u.LikedUsers).FirstOrDefaultAsync(u => u.Id == userId);
+        }
+    }
+}

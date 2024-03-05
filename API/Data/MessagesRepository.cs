6c3996af3e1b70aa2ec1fdfb34d41ab8fc50edcc
AKB-QuangVV12 <quangvv6@fpt.com>
2024-03-05 18:27:52 +0700

    .

---
+++ API/Data/MessagesRepository.cs
@@ -0,0 +1,49 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Helpers;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class MessagesRepository : IMessageRepository
+    {
+        private readonly DataContext _context;
+
+        public MessagesRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void AddMessage(Message message)
+        {
+            _context.Messages.Add(message);
+        }
+
+        public void DeleteMessage(Message message)
+        {
+            _context.Messages.Remove(message);
+        }
+
+        public async Task<Message> GetMessage(int id)
+        {
+            return await _context.Messages.FindAsync(id);
+        }
+
+        public Task<PagedList<MessageDto>> GetMessagesForUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<MessageDto>> GetMessagesThread(int currentUserId, int recipientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

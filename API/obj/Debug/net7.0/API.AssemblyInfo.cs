57b7262e86645fe7e65564263a2bd3a6b35cd988
AKB-QuangVV12 <quangvv6@fpt.com>
2024-04-02 18:07:01 +0700

    .

--- API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abb50fbfd5735c983bd4d19078bc3ecd24938659")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1bdd5fdcbbabcd567723baa6b4681ccd649409e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
